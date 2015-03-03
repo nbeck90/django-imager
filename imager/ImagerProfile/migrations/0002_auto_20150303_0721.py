ca8aa96ae52899de7bb2c9537b88a443ff70e24c
Nick Becker <nbeck90@uw.edu>
2015-03-03 09:12:48 -0800

    Getting Migration

---
+++ imager/ImagerProfile/migrations/0002_auto_20150303_0721.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ImagerProfile', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='imagerprofile',
+            name='profile_picture',
+            field=models.ImageField(null=True, upload_to=b''),
+            preserve_default=True,
+        ),
+    ]
