bcb578e01703351a83ff762077d1402cadcb64b7
James Warren <jwarren116@gmail.com>
2015-03-02 21:20:15 -0800

    Add ImagerProfile to extend standard User model in Admin interface

--- imager/ImagerProfile/admin.py
+++ imager/ImagerProfile/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from ImagerProfile.models import ImagerProfile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-admin.site.register(ImagerProfile)
+# admin.site.unregister(User)
+
+
+class ImagerUserInline(admin.StackedInline):
+    model = ImagerProfile
+    can_delete = False
+    verbose_name_plural = 'imager user'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (ImagerUserInline, )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- imager/ImagerProfile/migrations/0001_initial.py
+++ imager/ImagerProfile/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -17,11 +19,12 @@ class Migration(migrations.Migration):
                 ('profile_picture', models.ImageField(upload_to=b'')),
                 ('phone_number', models.CharField(max_length=15)),
                 ('birthday', models.DateField()),
-                ('picture_privacy', models.BooleanField()),
-                ('phone_privacy', models.BooleanField()),
-                ('birthday_privacy', models.BooleanField()),
-                ('name_privacy', models.BooleanField()),
-                ('email_privacy', models.BooleanField()),
+                ('picture_privacy', models.BooleanField(default=False)),
+                ('phone_privacy', models.BooleanField(default=False)),
+                ('birthday_privacy', models.BooleanField(default=False)),
+                ('name_privacy', models.BooleanField(default=False)),
+                ('email_privacy', models.BooleanField(default=False)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
