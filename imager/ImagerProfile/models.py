a533d6505956e67bb86e041d3c3e203ed326bc16
Nick Becker <nbeck90@uw.edu>
2015-03-03 10:11:08 -0800

    Adjust layout and add user

--- imager/ImagerProfile/models.py
+++ imager/ImagerProfile/models.py
@@ -2,20 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# class ImagerProfile(models.Manager):
-#     pass
-
-
 class ImagerProfile(models.Model):
     user = models.OneToOneField(User)
-    # objects = ImagerProfile()
 
     profile_picture = models.ImageField(null=True)
-    phone_number = models.CharField(max_length=15)  # X(XXX) XXX-XXXX
-    birthday = models.DateField()
-
     picture_privacy = models.BooleanField(default=False)
+
+    phone_number = models.CharField(max_length=15)  # X(XXX) XXX-XXXX
     phone_privacy = models.BooleanField(default=False)
+
+    birthday = models.DateField()
     birthday_privacy = models.BooleanField(default=False)
+
     name_privacy = models.BooleanField(default=False)
+
     email_privacy = models.BooleanField(default=False)
