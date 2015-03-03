173f874c4cf911fc9a35e0e039f164cb625fdccc
James Warren <jwarren116@gmail.com>
2015-03-03 12:56:38 -0800

    Add string representation of class, is_active method, and first draft of active class method

--- imager/ImagerProfile/models.py
+++ imager/ImagerProfile/models.py
@@ -1,9 +1,12 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class ImagerProfile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, related_name='profile')
 
     profile_picture = models.ImageField(null=True)
     picture_privacy = models.BooleanField(default=False)
@@ -17,3 +20,14 @@ class ImagerProfile(models.Model):
     name_privacy = models.BooleanField(default=False)
 
     email_privacy = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "User: {}".format(self.user.username)
+
+    def is_active(self):
+        return self.user.is_active()
+
+    @classmethod
+    def active(self):
+        qs = self.get_queryset()
+        return qs.filter(user__is_active=True)
