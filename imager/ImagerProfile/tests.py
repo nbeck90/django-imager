e632fa3e12d3627abaf26f41a9f0483aaea24adf
James Warren <jwarren116@gmail.com>
2015-03-03 16:49:49 -0800

    Change test UserFactory model to point to User

--- imager/ImagerProfile/tests.py
+++ imager/ImagerProfile/tests.py
@@ -4,7 +4,7 @@ import factory
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = 'imagerprofile.ImagerProfile'
+        model = 'imagerprofile.User'
         django_get_or_create = ('username',)
 
-    username = 'John'
+    username = factory.Sequence(lambda n: "Agent %03d" % n)
