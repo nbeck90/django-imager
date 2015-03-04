b0d58ec5f7dbf792705cdcefbbfd356b8b50b7fb
James Warren <jwarren116@gmail.com>
2015-03-03 18:54:45 -0800

    Outline Relationship model to deal with follows, friends, blocks

--- imager/ImagerProfile/models.py
+++ imager/ImagerProfile/models.py
@@ -6,9 +6,11 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class ImagerProfile(models.Model):
+
     user = models.OneToOneField(User, related_name='profile')
 
-    profile_picture = models.ImageField(null=True, blank=True, upload_to='images')
+    profile_picture = models.ImageField(null=True, blank=True,
+                                        upload_to='images')
     phone_number = models.CharField(max_length=15)  # X(XXX) XXX-XXXX
     birthday = models.DateField(blank=True)
 
@@ -18,11 +20,11 @@ class ImagerProfile(models.Model):
     name_privacy = models.BooleanField(default=False)
     email_privacy = models.BooleanField(default=False)
 
-    # relationship = models.ManyToManyField(User,
-    #                                       symetrical=False,
-    #                                       related_name='+',
-    #                                       through='imager_users.Relationship'
-    #                                       )
+    relationship = models.ManyToManyField('self',
+                                          symmetrical=False,
+                                          related_name='+',
+                                          through='imagerprofile.Relationship'
+                                          )
 
     # To find following
     # return Relationship.objects.filter(left=me, status__in=(1, 3))
@@ -47,8 +49,21 @@ class ImagerProfile(models.Model):
         qs = self.get_queryset()
         return qs.filter(user__is_active=True)
 
+
+@python_2_unicode_compatible
+class Relationship(models.Model):
+
+    default = 0
+    follow = 1
+    friend = 2
+    block = 4
+
+    relationship_to = models.ForeignKey(ImagerProfile)
+    relationship_from = models.ForeignKey(ImagerProfile)
+    status = models.IntegerField()
+
+
 # create profile
-# delete user
 
 # post_save.connect(create_profile, sender=User)
 # pre_delete.conect(delete_user, sender=ImagerProfile)
