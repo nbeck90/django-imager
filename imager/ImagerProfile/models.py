bc08ea65fb97e324e9704bba6e16a1169083c29f
James Warren <jwarren116@gmail.com>
2015-03-03 22:16:38 -0800

    Add first round of tests; comment out Relationship model to test ImagerProfile

--- imager/ImagerProfile/models.py
+++ imager/ImagerProfile/models.py
@@ -12,7 +12,7 @@ class ImagerProfile(models.Model):
     profile_picture = models.ImageField(null=True, blank=True,
                                         upload_to='images')
     phone_number = models.CharField(max_length=15)  # X(XXX) XXX-XXXX
-    birthday = models.DateField(blank=True)
+    birthday = models.DateField(null=True, blank=True)
 
     phone_privacy = models.BooleanField(default=False)
     birthday_privacy = models.BooleanField(default=False)
@@ -20,11 +20,11 @@ class ImagerProfile(models.Model):
     name_privacy = models.BooleanField(default=False)
     email_privacy = models.BooleanField(default=False)
 
-    relationship = models.ManyToManyField('self',
-                                          symmetrical=False,
-                                          related_name='+',
-                                          through='imagerprofile.Relationship'
-                                          )
+    # relationship = models.ManyToManyField('self',
+    #                                       symmetrical=False,
+    #                                       related_name='+',
+    #                                       through='imagerprofile.Relationship'
+    #                                       )
 
     # To find following
     # return Relationship.objects.filter(left=me, status__in=(1, 3))
@@ -50,20 +50,14 @@ class ImagerProfile(models.Model):
         return qs.filter(user__is_active=True)
 
 
-@python_2_unicode_compatible
-class Relationship(models.Model):
+# @python_2_unicode_compatible
+# class Relationship(models.Model):
 
-    default = 0
-    follow = 1
-    friend = 2
-    block = 4
+#     default = 0
+#     follow = 1
+#     friend = 2
+#     block = 4
 
-    relationship_to = models.ForeignKey(ImagerProfile)
-    relationship_from = models.ForeignKey(ImagerProfile)
-    status = models.IntegerField()
-
-
-# create profile
-
-# post_save.connect(create_profile, sender=User)
-# pre_delete.conect(delete_user, sender=ImagerProfile)
+#     relationship_to = models.ForeignKey(ImagerProfile)
+#     relationship_from = models.ForeignKey(ImagerProfile)
+#     status = models.IntegerField()
--- imager/ImagerProfile/tests.py
+++ imager/ImagerProfile/tests.py
@@ -1,10 +1,44 @@
 from django.test import TestCase
-import factory
+from django.contrib.auth.models import User
+from imagerprofile.models import ImagerProfile
+# import factory
 
 
-class UserFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = 'imagerprofile.User'
-        django_get_or_create = ('username',)
+# class UserFactory(factory.django.DjangoModelFactory):
+#     class Meta:
+#         model = 'imagerprofile.User'
+#         django_get_or_create = ('username',)
 
-    username = factory.Sequence(lambda n: "Agent %03d" % n)
+#     username = factory.Sequence(lambda n: "User%03d" % n)
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username='john_smith', is_active=False)
+        User.objects.create(username='tom_thompson',
+                            first_name='Tom', is_active=True)
+
+    def test_user_has_username(self):
+        john = User.objects.get(username='john_smith')
+        tom = User.objects.get(username='tom_thompson')
+        assert john.profile
+        assert tom.profile
+
+    def test_user_phone_number(self):
+        tom = User.objects.get(username='tom_thompson')
+        tom.profile.phone_number = '206-777-1234'
+        self.assertEqual(tom.profile.phone_number, '206-777-1234')
+
+    def test_user_active(self):
+        john = User.objects.get(username='john_smith')
+        tom = User.objects.get(username='tom_thompson')
+        assert tom.is_active
+        assert not john.is_active
+
+    def test_profile_default(self):
+        tom = User.objects.get(username='tom_thompson')
+        assert tom.profile.phone_privacy is False
+
+    def test_profile_active(self):
+        tom = User.objects.get(username='tom_thompson')
+        self.assertEqual(ImagerProfile.active(), tom)
