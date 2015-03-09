7d108170af47235f7cb4f5f1c517b875c372c881
James Warren <jwarren116@gmail.com>
2015-03-09 10:16:31 -0700

    Remove non-working code; update field lengths to reflect factors of two

--- imager/imager_images/models.py
+++ imager/imager_images/models.py
@@ -24,41 +24,41 @@ privacy_list = [(PUBLIC, 'Public'),
 class ImagerPhoto(models.Model):
     """image model"""
     user = models.ForeignKey(User, related_name='photos', null=True)
-    title = models.CharField(default='MyPhoto', max_length=20)
+    title = models.CharField(default='MyPhoto', max_length=31)
     picture = models.ImageField(upload_to='images')
     albums = models.ManyToManyField('imager_images.ImagerAlbum',
                                     related_name='photos',
                                     blank=True,
                                     null=True)
-    description = models.CharField(blank=True, max_length=20)
+    description = models.TextField(blank=True)
     date_uploaded = models.DateField(auto_now_add=True)
     date_published = models.DateField(null=True, blank=True)
     date_modified = models.DateField(auto_now=True)
     published = models.CharField(choices=privacy_list,
                                  default=PUBLIC,
-                                 max_length=20)
+                                 max_length=31)
 
     def __str__(self):
         return str(self.title)
 
-    def image_tag(self):
-        return u'<img src="/media/%s" width="50" height="50"/>' % self.picture
-    image_tag.short_description = description
-    image_tag.allow_tags = True
+    # def image_tag(self):
+    #     return u'<img src="/media/%s" width="50" height="50"/>' % self.picture
+    # image_tag.short_description = description
+    # image_tag.allow_tags = True
 
 
 @python_2_unicode_compatible
 class ImagerAlbum(models.Model):
     """photo album model"""
     user = models.ForeignKey(User, related_name='albums', null=True)
-    title = models.CharField(default='MyAlbum', max_length=50)
+    title = models.CharField(default='MyAlbum', max_length=63)
     description = models.TextField(blank=True)
     date_created = models.DateField(auto_now_add=True)
     date_published = models.DateField(null=True, blank=True)
     date_modified = models.DateField(auto_now=True)
     published = models.CharField(choices=privacy_list,
                                  default=PUBLIC,
-                                 max_length=20)
+                                 max_length=7)
     cover = models.ForeignKey(ImagerPhoto,
                               blank=True,
                               related_name='+',
@@ -66,12 +66,6 @@ class ImagerAlbum(models.Model):
 
     # objects = models.Manager()
     # photos = ImagerPhoto()
-
-    def add(self, photo):
-        if self.user is photo.user:
-            photo.albums.add(self)
-        else:
-            raise AttributeError
 
     def __str__(self):
         return str(self.title)
