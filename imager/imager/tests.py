f5c56035737bc3a647a8f8c5e8a57ebfe7f2bce9
James Warren <jwarren116@gmail.com>
2015-03-11 14:31:34 -0700

    Add view tests for login, register and fix login helper

--- imager/imager/tests.py
+++ imager/imager/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from django.core import mail
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from imagerprofile.models import ImagerProfile
@@ -8,10 +9,11 @@ import factory
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
-        django_get_or_create = ('username', 'password')
+        django_get_or_create = ('username', 'password', 'is_active')
 
     username = factory.Sequence(lambda n: "User%03d" % n)
     password = 'secret'
+    is_active = True
 
 
 class ProfileFactory(factory.django.DjangoModelFactory):
@@ -27,21 +29,35 @@ class LoginTestCase(TestCase):
         """set up basic client"""
         self.client = Client()
 
-    # def loginHelper(self):
-        # UserFactory(username='fred', password='secret')
-        # return self.client.post('/login', {'username': 'fred',
-        #                                    'password': 'secret'}
-        #                         )
+    def loginHelper(self, username, password):
+        return self.client.post('/login', {'username': username,
+                                           'password': password})
 
     def test_home(self):
         """test that home page is available to logged out user"""
         response = self.client.get(reverse('home'))
         assert response.status_code == 200
 
+    def test_register(self):
+        """test registration is available, and redirects on post"""
+        username = 'sally'
+        email = 'sally@sally.com'
+        password = 'secret'
+        response = self.client.get(reverse('registration_register'))
+        assert response.status_code == 200
+        response = self.client.post(
+            reverse('registration_register'), {
+                'username': username,
+                'email': email,
+                'password': password
+            })
+        assert response.status_code == 200
+
     def test_login(self):
         """test login is reachable when not logged in,
         login form changes auth"""
-        UserFactory(username='fred', password='secret')
         response = self.client.get(reverse('login'))
         assert response.status_code == 200
-        assert self.client.login(username='fred', password='secret')
+        fred = UserFactory(username='fred', password='secret')
+        self.loginHelper('fred', 'secret')
+        assert fred.is_authenticated()
