b1343972d5887853a938813e0a48c650ff172d02
James Warren <jwarren116@gmail.com>
2015-03-13 10:04:57 -0700

    Add url and view to delete album

--- imager/imager_images/urls.py
+++ imager/imager_images/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
-from imager_images.views import AlbumCreate, AlbumUpdate, PhotoCreate, PhotoDelete
+from imager_images.views import (AlbumCreate, AlbumUpdate, AlbumDelete,
+                                 PhotoCreate, PhotoDelete)
 
 
 urlpatterns = patterns('imager_images.views',
@@ -8,4 +9,5 @@ urlpatterns = patterns('imager_images.views',
     url(r'^delete/(?P<id>\d+)/$', login_required(PhotoDelete.as_view()), name='delete'),
     url(r'^album_create/$', login_required(AlbumCreate.as_view()), name='album_create'),
     url(r'^album_update/(?P<id>\d+)/$', login_required(AlbumUpdate.as_view()), name='album_update'),
+    url(r'^album_delete/(?P<id>\d+)/$', login_required(AlbumDelete.as_view()), name='album_delete'),
 )
--- imager/imager_images/views.py
+++ imager/imager_images/views.py
@@ -32,7 +32,13 @@ class AlbumUpdate(UpdateView):
     ]
 
 
+class AlbumDelete(DeleteView):
+    model = ImagerAlbum
+    success_url = '/profile/'
+
+
 class PhotoCreate(CreateView):
+    # queryset = ImagerPhoto.objects.filter(albums__user=user)
     model = ImagerPhoto
     context_object_name = 'photo'
     success_url = '/profile/'
@@ -47,6 +53,12 @@ class PhotoCreate(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(PhotoCreate, self).form_valid(form)
+
+    # def get_context_data(self, **kwargs):
+    #     user = self.request.user
+    #     context = super(PhotoCreate, self).get_context_data(**kwargs)
+    #     context['albums'] = ImagerAlbum.objects.filter(user=user).all()
+    #     return context
 
 
 class PhotoDelete(DeleteView):
