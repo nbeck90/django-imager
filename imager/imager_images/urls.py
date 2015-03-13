746439a977cd556e91424c80cf532e1da8551ae7
James Warren <jwarren116@gmail.com>
2015-03-13 15:14:27 -0700

    Change url routes to use primary key instead of id

--- imager/imager_images/urls.py
+++ imager/imager_images/urls.py
@@ -6,8 +6,8 @@ from imager_images.views import (AlbumCreate, AlbumUpdate, AlbumDelete,
 
 urlpatterns = patterns('imager_images.views',
     url(r'^upload/$', login_required(PhotoCreate.as_view()), name='upload'),
-    url(r'^delete/(?P<id>\d+)/$', login_required(PhotoDelete.as_view()), name='delete'),
+    url(r'^delete/(?P<pk>\d+)/$', login_required(PhotoDelete.as_view()), name='delete'),
     url(r'^album_create/$', login_required(AlbumCreate.as_view()), name='album_create'),
-    url(r'^album_update/(?P<id>\d+)/$', login_required(AlbumUpdate.as_view()), name='album_update'),
-    url(r'^album_delete/(?P<id>\d+)/$', login_required(AlbumDelete.as_view()), name='album_delete'),
+    url(r'^album_update/(?P<pk>\d+)/$', login_required(AlbumUpdate.as_view()), name='album_update'),
+    url(r'^album_delete/(?P<pk>\d+)/$', login_required(AlbumDelete.as_view()), name='album_delete'),
 )
