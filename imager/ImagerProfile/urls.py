bae4da08e007635d89271c7ff7344a09d22c5db1
James Warren <jwarren116@gmail.com>
2015-03-14 12:07:03 -0700

    Add view and url route to see profile of another user

--- imager/ImagerProfile/urls.py
+++ imager/ImagerProfile/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from imagerprofile.views import ImagerProfileUpdateView, profile
+from imagerprofile.views import ImagerProfileUpdateView, profile, user_profile
 
 urlpatterns = patterns('',
     url(r'^$', profile, name='my_profile'),
-    # url(r'^(?P<id>\d+)/', user_profile, name='profile'),
-    url(r'^(?P<username>\w+)/edit/', ImagerProfileUpdateView.as_view(), name='profile_edit'),
+    url(r'^(?P<id>\d+)/', user_profile, name='profile'),
+    url(r'^(?P<username>\w+)/edit/',
+        ImagerProfileUpdateView.as_view(), name='profile_edit'),
 )
--- imager/ImagerProfile/views.py
+++ imager/ImagerProfile/views.py
@@ -17,7 +17,6 @@ class ImagerProfileUpdateView(UpdateView):
 
 @login_required()
 def profile(request):
-    # import pdb; pdb.set_trace()
     user = request.user
     try:
         albums = ImagerAlbum.objects.filter(user=user).all()
@@ -27,6 +26,26 @@ def profile(request):
     following = len(profile.followers.all())
     num_photos = ImagerPhoto.objects.filter(user=user).count()
     return render(request, 'profile.html', {
+        'albums': albums,
+        'user': user,
+        'profile': profile,
+        'following': following,
+        'num_album': len(albums),
+        'num_photo': num_photos
+    })
+
+
+@login_required()
+def user_profile(request, id):
+    user = User.objects.get(id=id)
+    try:
+        albums = ImagerAlbum.objects.filter(user=user).all()
+    except:
+        albums = None
+    profile = ImagerProfile.objects.get(user=user)
+    following = len(profile.followers.all())
+    num_photos = ImagerPhoto.objects.filter(user=user).count()
+    return render(request, 'user_profile.html', {
         'albums': albums,
         'user': user,
         'profile': profile,
