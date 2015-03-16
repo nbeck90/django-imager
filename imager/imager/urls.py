d34950e974b8832b3d2a7ea5cda97d1992e7268c
James Warren <jwarren116@gmail.com>
2015-03-16 11:12:33 -0700

    Change static url to only be added in debug mode

--- imager/imager/urls.py
+++ imager/imager/urls.py
@@ -20,5 +20,7 @@ urlpatterns = patterns('',
     url(r'^$', 'imager.views.home', name='home'),
     url(r'^profile/', include('imagerprofile.urls')),
     url(r'^images/', include('imager_images.urls')),
+)
 
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
