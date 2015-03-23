b51483ae8ef6dd3d02a2475c9a59b5e1765c174d
James Warren <jwarren116@gmail.com>
2015-03-22 17:45:00 -0700

    Add additional Selenium tests to cover album add/update/delete and logouts

--- imager/imager/tests.py
+++ imager/imager/tests.py
@@ -6,6 +6,7 @@ from imagerprofile.models import ImagerProfile
 from imager_images.tests import ImageFactory, AlbumFactory
 import factory
 from selenium import webdriver
+from time import sleep
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -13,7 +14,7 @@ class UserFactory(factory.django.DjangoModelFactory):
         model = User
         django_get_or_create = ('username', 'password', 'is_active')
 
-    username = factory.Sequence(lambda n: "User%03d" % n)
+    username = factory.Sequence(lambda n: 'User%03d' % n)
     password = 'secret'
     is_active = True
 
@@ -28,7 +29,7 @@ class ProfileFactory(factory.django.DjangoModelFactory):
 
 # class LoginTestCase(TestCase):
 #     # def setUp(self):
-#     #     """set up basic client"""
+#     #     '''set up basic client'''
 #     #     self.client = Client()
 
 #     def loginHelper(self, username, password):
@@ -36,12 +37,12 @@ class ProfileFactory(factory.django.DjangoModelFactory):
 #                                            'password': password})
 
 #     def test_home(self):
-#         """test that home page is available to logged out user"""
+#         '''test that home page is available to logged out user'''
 #         response = self.client.get(reverse('home'))
 #         self.assertEqual(response.status_code, 200)
 
 #     def test_register(self):
-#         """test registration is available, and redirects on post"""
+#         '''test registration is available, and redirects on post'''
 #         username = 'sally'
 #         email = 'sally@sally.com'
 #         password = 'secret'
@@ -56,8 +57,8 @@ class ProfileFactory(factory.django.DjangoModelFactory):
 #         self.assertEqual(response.status_code, 200)
 
 #     def test_login(self):
-#         """test login is reachable when not logged in,
-#         login changes auth"""
+#         '''test login is reachable when not logged in,
+#         login changes auth'''
 #         response = self.client.get(reverse('login'))
 #         self.assertEqual(response.status_code, 200)
 #         fred = UserFactory(username='fred', password='secret')
@@ -65,14 +66,14 @@ class ProfileFactory(factory.django.DjangoModelFactory):
 #         assert fred.is_authenticated()
 
     # def test_profile_view_redirect(self):
-    #     """unauthenticated user redirected when trying
-    #     to view personal profile"""
+    #     '''unauthenticated user redirected when trying
+    #     to view personal profile'''
     #     # response = self.client.get('/logout')
     #     response = self.client.get('/profile')
     #     self.assertRedirects(response, '/login/', status_code=302)
 
     # def test_profile_view(self):
-    #     """authenticated user able to view personal profile"""
+    #     '''authenticated user able to view personal profile'''
     #     fred = UserFactory(username='fred', password='secret')
     #     response = self.loginHelper('fred', 'secret')
     #     assert fred.is_authenticated()
@@ -90,7 +91,12 @@ class TestProfileView(LiveServerTestCase):
             )
         self.user1.set_password('secret')
         self.user1.save()
-        self.user2 = UserFactory.create(username='bob')
+        self.user2 = UserFactory.create(
+            username='bob',
+            first_name='Bob',
+            )
+        self.user2.set_password('secret')
+        self.user2.save()
         self.selenium = webdriver.Firefox()
         super(TestProfileView, self).setUp()
 
@@ -104,61 +110,89 @@ class TestProfileView(LiveServerTestCase):
         self.selenium.get(
             '{}{}'.format(self.live_server_url, reverse('login'))
             )
-        self.selenium.find_element_by_id('id_username').send_keys('alice')
+        self.selenium.find_element_by_id('id_username').send_keys(user.username)
         self.selenium.find_element_by_id('id_password').send_keys('secret')
         self.selenium.find_element_by_tag_name('form').submit()
 
-    # def upload_photo(self, user):
-    #     pass
+    def create_album(self, user=None):
+        if user is None:
+            user = self.user2
+            self.login_user(user)
+        self.selenium.get(
+            '{}{}'.format(self.live_server_url, reverse('album_create'))
+            )
+        self.selenium.find_element_by_id('id_title').clear()
+        self.selenium.find_element_by_id('id_title').send_keys('My test album')
+        self.selenium.find_element_by_id('id_description').send_keys('This is an album test')
+        self.selenium.find_element_by_id('submit').click()
 
     def test_login_redirect(self):
         user = self.user1
         self.login_user(user)
         assert user.is_authenticated()
         # self.selenium.save_screenshot('login.png')
-        assert self.selenium.find_element_by_id("stream_link")
-        assert self.selenium.find_element_by_partial_link_text("Update my profile")
+        assert self.selenium.find_element_by_id('stream_link')
+        assert self.selenium.find_element_by_partial_link_text('Update my profile')
 
     def test_profile(self):
         user = self.user1
         self.login_user(user)
         # self.selenium.save_screenshot('profile.png')
-        assert self.selenium.find_element_by_id("stream_link")
-        assert self.selenium.find_element_by_id("welcome_name")
-        assert self.selenium.find_element_by_class_name("profile_pic")
+        assert self.selenium.find_element_by_id('stream_link')
+        assert self.selenium.find_element_by_id('welcome_name')
+        assert self.selenium.find_element_by_class_name('profile_pic')
 
     def test_stream(self):
         user = self.user1
         self.login_user(user)
-        self.selenium.find_element_by_partial_link_text("Stream").click()
+        self.selenium.find_element_by_partial_link_text('Stream').click()
         # self.selenium.save_screenshot('stream.png')
-        assert self.selenium.find_element_by_id("album_gallery")
-        assert self.selenium.find_element_by_id("photo_gallery")
+        assert self.selenium.find_element_by_id('album_gallery')
+        assert self.selenium.find_element_by_id('photo_gallery')
 
     def test_library(self):
         user = self.user1
         self.login_user(user)
-        self.selenium.find_element_by_partial_link_text("Library").click()
+        self.selenium.find_element_by_partial_link_text('Library').click()
         # self.selenium.save_screenshot('library.png')
-        assert self.selenium.find_element_by_id("album_gallery")
-        assert self.selenium.find_element_by_id("photo_gallery")
+        assert self.selenium.find_element_by_id('album_gallery')
+        assert self.selenium.find_element_by_id('photo_gallery')
 
     def test_upload_photo(self):
         user = self.user1
         self.login_user(user)
-        self.selenium.find_element_by_partial_link_text("Upload Photo").click()
+        self.selenium.find_element_by_partial_link_text('Upload Photo').click()
         self.selenium.find_element_by_id('id_picture').send_keys('/Users/jwarren/Desktop/jwarren.jpg')
         # self.selenium.save_screenshot('upload.png')
         self.selenium.find_element_by_id('submit').click()
         assert self.selenium.find_element_by_tag_name('img')
 
-
     def test_create_album(self):
-        user = self.user1
+        user = self.user2
         self.login_user(user)
-        self.selenium.find_element_by_partial_link_text("Create Album").click()
-        self.selenium.find_element_by_id('id_title').send_keys('My test album')
-        self.selenium.find_element_by_id('id_description').send_keys('This is an album test')
+        self.create_album(user)
         # self.selenium.save_screenshot('create.png')
+        assert self.selenium.find_element_by_class_name('library_gallery')
+
+    def test_edit_and_delete_album(self):
+        user = self.user2
+        self.login_user(user)
+        self.create_album(user)
+        self.selenium.get(
+            '{}{}'.format(self.live_server_url, reverse('library'))
+            )
+        self.selenium.find_element_by_class_name('library_gallery').click()
+        self.selenium.find_element_by_id('id_published').send_keys('Private')
         self.selenium.find_element_by_id('submit').click()
-        assert self.selenium.find_element_by_tag_name('img')
+        self.selenium.find_element_by_class_name('library_gallery').click()
+        # self.selenium.save_screenshot('pre_delete.png')
+        self.selenium.find_element_by_name('album_delete').click()
+        # self.selenium.save_screenshot('post_delete.png')
+        assert self.selenium.find_element_by_id('no_albums')
+
+    def test_logout(self):
+        user = self.user2
+        self.login_user(user)
+        self.selenium.find_element_by_link_text('Logout').click()
+        assert self.selenium.find_element_by_link_text('Login')
+        assert self.selenium.find_element_by_link_text('Register')
